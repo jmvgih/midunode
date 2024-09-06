3a52df99ed64780b2fe803688bcb4194110211b4
Jacinto Munoz Vivas <jomzvs@gmail.com>
2024-09-06 20:08:18 +0200

    comienzo Creando el POST

--- clase-03/app.js
+++ clase-03/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const crypto = require('node:crypto')
 
 const app = express()
 app.disable('x-powered-by')
+app.use(express.json())
 
 const movies = require('./movies.json')
 
@@ -24,6 +26,35 @@ app.get('/movies/:id',(req,res)=>{
     const movie = movies.find(movie => movie.id === id)
     if (movie) return res.json(movie)
     res.status(400).json({message:"Movie not found"})
+})
+
+app.post('/movies', (req, res)=>{
+    const {
+    title,
+    year,
+    director,
+    duratio,
+    poster,
+    genre,
+    rate,
+    } = req.body
+
+    const newMovie = {
+        id: crypto.randomUUID(), //Crea uuid v4
+        title,
+        year,
+        director,
+        duratio,
+        poster,
+        genre,
+        rate: rate ?? 0,
+    }
+
+    //Esto no sería REST al estar guardando información en el servidor. Más adelante iremos a la base de datos
+    movies.push(newMovie)
+
+    res.status(201).json(newMovie)
+
 })
 
 const PORT = process.env.PORT ?? 1234
