24dc40d1ec6ad6c9c2cd8b63a407348ce01fc039
Jacinto Munoz Vivas <jomzvs@gmail.com>
2024-09-10 19:01:09 +0200

    Comenzando clase 04

---
+++ clase-04/app.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const cors = require('cors')
+const crypto = require('node:crypto')
+
+const app = express()
+app.disable('x-powered-by')
+app.use(express.json())
+app.use(cors())  //soluciona el CORS poniendo * en las cabeceras Tiene opciones para limitarlas
+
+const movies = require('./movies.json')
+const { validateMovie } = require('./schemas/movies')
+const { validatePartialMovie } = require('./schemas/movies')
+
+const ACCEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'http://movies.com',
+  'http://jmv.com'
+
+]
+
+//Todos los recursos que sean movies, se identifican con esta URL /movies
+app.get('/movies', (req, res) => {
+ 
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+
+
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(400).json({ message: "Movie not found" })
+})
+
+app.post('/movies', (req, res) => {
+
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(), //Crea uuid v4
+    ...result.data
+  }
+
+  //Esto no sería REST al estar guardando información en el servidor. Más adelante iremos a la base de datos
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
+
+})
+
+app.delete('/movies/:id', (req, res)  =>{
+
+  const {id} = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+  return res.status(200).json({messgae: 'Movie deleted'})
+
+})
+
+app.patch('/movies/:id', (req, res) => {
+  
+  const result = validatePartialMovie(req.body)
+  if (!result.success){
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.status(200).json(updateMovie)
+
+})
+
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`Escuchando en http://localhost:${PORT}`)
+})
---
+++ clase-04/schemas/movies.js
@@ -0,0 +1,28 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required' // Y otras muchas posibilidades
+  }),
+  year: z.number().int().min(1900).max(2024), //Positive está como ejemplo :)
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).nullable().default(null),
+  poster: z.string().url(),
+  //genre: z.array(s.string()) Tambien se puede limitar con enum
+  genre: z.array(z.enum(['Adventure', 'Sci-Fi', 'Action', 'Crime', 'Drama', 'Romance', 'Biography', 'Fantasy', 'Comedy', 'Horror', 'Thriller']))
+})
+
+function validateMovie(object) {
+  return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie(object){
+  return movieSchema.partial().safeParse(object)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
