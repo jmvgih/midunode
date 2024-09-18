c443c89095855073083b47964b1955cc97c8c0cc
Jacinto Munoz Vivas <jomzvs@gmail.com>
2024-09-18 20:13:50 +0200

    Finaalizando el modelo para MySQL

--- clase-05/models/mysql/movie.js
+++ clase-05/models/mysql/movie.js
@@ -17,7 +17,7 @@ export class MovieModel {
 
       // get genre ids from database table using genre names
       const [genres] = await connection.query(
-        'SELECT id, name FROM genre WHERE LOWER(name) = ?;',
+        'SELECT id, name FROM genres WHERE LOWER(name) = ?;',
         [lowerCaseGenre]
       )
 
@@ -41,18 +41,121 @@ export class MovieModel {
   }
 
   static async getById ({ id }) {
+    const [movie] = await connection.query(
+      'SELECT title, year, director, duration, poster, BIN_TO_UUID(id) id FROM movies WHERE id= UUID_TO_BIN(?);',
+      [id]
+    )
 
+    if (movie.le === 0) return null
+    return movie[0]
   }
 
-  static async create ({ input }) {
+  static async create({ input }) {
+    const {
+      genre: genreInput, //genre is an array
+      title,
+      year,
+      director,
+      duration,
+      poster,
+      rate
+    } = input
 
+    //Todo: crear conexión de genre
+
+    const [uuidResult] = await connection.query('SELECT UUID() uuid;')
+    const [{ uuid }] = uuidResult
+
+    try {
+      await connection.query(
+        `INSERT INTO movies (id, title, year, director, duration, poster, rate)
+          VALUES (UUID_TO_BIN(?), ?, ?, ?, ?, ?, ?);`,
+        [uuid, title, year, director, duration, poster, rate]
+      )
+    } catch (e) {
+      //return e
+      // no mandar error al usuario puede enviarle información sensible
+      throw new Error('Error creating movie')
+      // enviar la traza a un servicio interno
+      // sendLog(e)
+    }
+
+    const [movie] = await connection.query(
+      `SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id
+        FROM movies WHERE id = UUID_TO_BIN(?);`,
+      [uuid]
+    )
+
+    return movie[0]
   }
+  
 
   static async delete ({ id }) {
+    try {
+      const [movie] = await connection.query(
+        `SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id
+          FROM movies WHERE id = UUID_TO_BIN(?);`,
+        [id]
+      )
+      if (movie.length === 0) return false
+
+      await connection.query(
+        'DELETE FROM movies WHERE id= UUID_TO_BIN(?);',
+      [id]
+      )      
+    } catch (e) {
+      return e
+      // no mandar error al usuario puede enviarle información sensible
+      //throw new Error('Error deleting movie')
+      // enviar la traza a un servicio interno
+      // sendLog(e)
+    }
+
+    return true
 
   }
 
-  static async update ({ id, input }) {
+  static async update({ id, input }) {
+   
+    const [movie] = await connection.query(
+      `SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id
+        FROM movies WHERE id = UUID_TO_BIN(?);`,
+      [id]
+    )
+    if (movie.length === 0) return false
+
+    const movieUpdate = {
+      ...movie[0],
+      ...input
+    }
+    console.log(movieUpdate)
+
+    const {
+      genre: genreInput, //genre is an array
+      title,
+      year,
+      director,
+      duration,
+      poster,
+      rate
+    } = movieUpdate
+    console.log(movieUpdate)
+
+    try {
+
+      await connection.query(
+        'UPDATE movies SET title=?, year= ?,director= ?, duration= ?, poster= ?, rate= ? WHERE id= UUID_TO_BIN(?);',
+        [title, year, director, duration, poster, rate, id]
+      )
+    } catch (e) {
+      return e
+      // no mandar error al usuario puede enviarle información sensible
+      //throw new Error('Error deleting movie')
+      // enviar la traza a un servicio interno
+      // sendLog(e)
+    }
+
+    return true
 
   }
 }
