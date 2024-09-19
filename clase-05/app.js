915f0e3513d462aa0b17eabaa4270db2a2b64225
Jacinto Munoz Vivas <jomzvs@gmail.com>
2024-09-19 21:03:44 +0200

    Modificado para incluir inyección de dependencias

--- clase-05/app.js
+++ clase-05/app.js
@@ -1,19 +1,25 @@
 import express, { json } from 'express'
-import { moviesRouter } from './routes/movies.js'
+import { createMovieRouter } from './routes/movies.js'
 import { corsMiddleware } from './middlewares/cors.js'
 
-const app = express()
-app.disable('x-powered-by')
-app.use(json())
-app.use(corsMiddleware())  //soluciona el CORS poniendo * en las cabeceras Tiene opciones para limitarlas
+export const createApp = ({movieModel}) => {
+  const app = express()
+  app.disable('x-powered-by')
+  app.use(json())
+  app.use(corsMiddleware())  //soluciona el CORS poniendo * en las cabeceras Tiene opciones para limitarlas
+  
+  
+  //Todos los recursos que sean movies, se identifican con esta URL /movies
+  app.use('/movies', createMovieRouter({ movieModel }))
+  
+  const PORT = process.env.PORT ?? 1234
+  
+  app.listen(PORT, () => {
+    console.log(`App escuchando en http://localhost:${PORT}`)
+  })
+
+  return app
+}
 
 
-//Todos los recursos que sean movies, se identifican con esta URL /movies
-app.use('/movies', moviesRouter)
-
-const PORT = process.env.PORT ?? 1234
-
-app.listen(PORT, () => {
-  console.log(`App escuchando en http://localhost:${PORT}`)
-})
 
